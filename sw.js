c45f166c0422e71a8aa6aefe72d11b8543ce2527
isikdev <isikjandev@gmail.com>
2024-11-11 18:35:08 +0500

    101%

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "pwa-cache-v1";
+const URLS_TO_CACHE = [
+    "/"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(URLS_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return (
+                response ||
+                fetch(event.request).then((networkResponse) => {
+                    return caches.open(CACHE_NAME).then((cache) => {
+                        cache.put(event.request, networkResponse.clone());
+                        return networkResponse;
+                    });
+                })
+            );
+        }).catch(() => {
+            return caches.match("https://willway.pro/");
+        })
+    );
+});
